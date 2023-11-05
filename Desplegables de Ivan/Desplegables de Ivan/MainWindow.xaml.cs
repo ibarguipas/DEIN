d3c99da1f018e0ed8ac174a6e4be4ceeb576337e
ibarguipas <ibarguipas@educacion.navarra.es>
2023-11-05 20:27:29 +0100

    Añadido menus y comportamiento de botones

--- Desplegables de Ivan/Desplegables de Ivan/MainWindow.xaml.cs
+++ Desplegables de Ivan/Desplegables de Ivan/MainWindow.xaml.cs
@@ -24,5 +24,65 @@ namespace Desplegables_de_Ivan
         {
             InitializeComponent();
         }
+
+        private void M_Nuevo_CLick(object sender, RoutedEventArgs e)
+        {
+            MainWindow NuevaVEntana = new MainWindow();
+            NuevaVEntana.Show();
+        }
+
+        private void M_Abrir_CLick(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog AbrirFichero = new Microsoft.Win32.OpenFileDialog();
+            AbrirFichero.ShowDialog();
+        }
+
+        private void M_Guardar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(" Guardado ");
+        }
+
+        private void M_Guardar_como_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog GuardarFicheroComo = new Microsoft.Win32.SaveFileDialog();
+            GuardarFicheroComo.ShowDialog();
+        }
+
+        private void M_Imprimir_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.PrintDialog Imprimir = new System.Windows.Controls.PrintDialog();
+            Imprimir.ShowDialog();
+        }
+        private void M_Salir_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void M_Usuarios_Click(object sender, RoutedEventArgs e)
+        {
+            Usuarios AbrirUsuarios = new Usuarios();
+            AbrirUsuarios.Show();
+        }
+
+        private void M_Copiar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(" Copiado ");
+        }
+
+        private void M_Cortar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(" Cortado ");
+        }
+
+        private void M_Pegar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(" Pegado ");
+        }
+
+        private void M_Eliminar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(" Eliminado ");
+        }
+
     }
 }
