cdad1b3a1710d527ca8c96336af69f243d21c12f
ibarguipas <ibarguipas@educacion.navarra.es>
2023-11-23 19:19:18 +0100

    estilos y menus

--- proyectoIvan/MainWindow.xaml.cs
+++ proyectoIvan/MainWindow.xaml.cs
@@ -24,5 +24,15 @@ namespace proyectoIvan
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
